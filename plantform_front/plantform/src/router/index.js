dd34660e6f8a9ae4d7ca697132c2966cf60973de
zc <zhangchanget@126.com>
2021-07-05 18:57:15 +0800

    07.05添加VUE主页

--- plantform_front/plantform/src/router/index.js
+++ plantform_front/plantform/src/router/index.js
@@ -15,6 +15,11 @@ const routes = [
     path: '/signUp',
     name: 'SignUp',
     component: SignUp
+  },
+  {
+    path: '/main',
+    name: 'Main',
+    component: Main
   }
   // {
   //   path: '/about',
