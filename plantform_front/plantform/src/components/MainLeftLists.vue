b0fdcfd1105e7bbe1f5da8204a0456efe860e1ca
zc <zhangchanget@126.com>
2021-07-14 11:58:21 +0800

    07.13新建用例页面上传

---
+++ plantform_front/plantform/src/components/MainLeftLists.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="300"
+    tile
+  >
+    <v-list rounded>
+      <v-subheader>REPORTS</v-subheader>
+      <v-list-item-group
+        v-model="selectedItem"
+        color="primary"
+      >
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          @click="click_button(item)"
+        >
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+
+
+<script>
+  export default {
+    data: () => ({
+      selectedItem: 1,
+      items: [
+        { text: '测试用例', icon: 'mdi-clock' ,router: "TestCase"},
+        { text: '测试任务', icon: 'mdi-account' },
+        { text: '测试报告', icon: 'mdi-flag' ,router: "TestReport"},
+      ],
+    }),
+    methods:{
+        click_button(item){
+            console.log(item)
+            this.$router.push({"name": item.router})
+        }
+    }
+  }
+</script>
